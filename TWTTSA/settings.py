9767c7433015bc13b0e93bd1c21584b74a542a72
Jonas Ferrao <74498509+projectultra@users.noreply.github.com>
2023-04-15 19:36:21 +0530

    Update settings.py

--- TWTTSA/settings.py
+++ TWTTSA/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
