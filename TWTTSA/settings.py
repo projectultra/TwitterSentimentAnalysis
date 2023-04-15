98abd26231c5989934c6324cdaef8bbbc1320079
Jonas Ferrao <74498509+projectultra@users.noreply.github.com>
2023-04-15 19:05:58 +0530

    Update settings.py

--- TWTTSA/settings.py
+++ TWTTSA/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-[label django_app/settings.py]
+
 from django.core.management.utils import get_random_secret_key
 
 from pathlib import Path
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-[label django_app/settings.py]
+
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
