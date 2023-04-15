2c079bc1316d019c6024016ddae16954fbf4f195
Jonas Ferrao <74498509+projectultra@users.noreply.github.com>
2023-04-15 20:06:59 +0530

    Update test.py

--- twstapp/test.py
+++ twstapp/test.py
@@ -10,10 +10,9 @@ since_date = "2022-01-01"
 until_date = "2022-01-07"
 
 # Create a query string for snscrape
-query = f"{search_words} since:{since_date} until:{until_date}"
 count=10
-mlmodel=keras.models.load_model(r'C:\Users\jonas\Desktop\Code\twstapp\mlmodels')
-file = open(r'C:\Users\jonas\Desktop\Code\twstapp\vocab.pkl','rb')
+mlmodel=keras.models.load_model(r'twstapp\models')
+file = open(r'twstapp\vocab.pkl','rb')
 vocab=pickle.load(file)
 vectorizer = keras.layers.TextVectorization(max_tokens=10000, output_mode='int', output_sequence_length=20, vocabulary=vocab)
 
@@ -37,7 +36,7 @@ def preprocess(tweet):
     tweet = ' '.join([lemmatizer.lemmatize(w) for w in tokenized_tweet])
     return tweet
 
-for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+for tweet in sntwitter.TwitterSearchScraper(search_words,mode=sntwitter.TwitterSearchScraperMode.LIVE).get_items():
     text=preprocess(tweet.__dict__['rawContent'])
     v=vectorizer([text])
     sentiment=mlmodel.predict(np.array(v))
